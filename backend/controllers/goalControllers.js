bb06c7d50b68b738afe564fbb27243a21dc0cfdd
Hamzeh Amayreh01~ <hamzehamayreh57@gmail.com>
2022-09-19 21:14:03 +0300

    initial REST API for goals

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,29 +1,46 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+
 const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
+});
+
+const setGoals = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "get goals" });
-});
-
-const setGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    Message: "set goals",
+  const goal = await Goal.create({
+    text: req.body.text,
   });
+  res.status(200).json(goal);
 });
 
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    Message: `update ${req.params.id}`,
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    throw new Erorr("Goal not found");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
   });
+  res.status(200).json(updatedGoal);
 });
 
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    Message: `deleting ${req.params.id}`,
-  });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    throw new Error("Cannot find goal");
+  }
+
+  await goal.remove();
+
+  res.status(200).json(req.params.id);
 });
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000; // access the port from .env file or 5000
+const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
+
 const app = express();
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
